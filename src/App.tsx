31497cbc6d8ce4e92a5ed4532313a801b50a0d4b
e1q23079 <e1q23079@oit.ac.jp>
2025-09-13 09:59:56 +0900

    intern-arhive作成

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { title } from './style.css.ts'
+
+const App = () => {
+  return (
+    <>
+      <div className={title}>
+        Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/style.css.ts
@@ -0,0 +1,8 @@
+import { style } from "@vanilla-extract/css";
+
+// タイトル
+export const title = style({
+    fontSize: "24px",
+    fontWeight: "bold",
+    marginBottom: "16px",
+});
