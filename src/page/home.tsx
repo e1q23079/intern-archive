24708f750463b053709c419782f5b0d90cd23945
e1q23079 <e1q23079@oit.ac.jp>
2025-09-21 15:23:06 +0900

    バックボタン，画面遷移

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -5,10 +5,17 @@ import sampleDesign from "../assets/sampleDesign/sampleDesign.png";
 import { backScreen } from "./start.css";
 
 const HomePage = () => {
+
+    const start = () => {
+        setTimeout(() => {
+            window.location.href = "#"
+        }, 1000);
+    }
+
     return (
         <>
             <img src={sampleDesign} alt="start screen back 02" className={backScreen} draggable={false} />
-            <div className={backBtnStyle}>
+            <div className={backBtnStyle} onClick={start}>
                 <BackBtn></BackBtn>
             </div>
         </>
