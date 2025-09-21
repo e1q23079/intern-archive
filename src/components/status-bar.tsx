909265e898720654130c39e93652d9b076c32f04
e1q23079 <e1q23079@oit.ac.jp>
2025-09-21 17:58:56 +0900

    スキル値を取得

--- src/components/status-bar.tsx
+++ src/components/status-bar.tsx
@@ -2,11 +2,24 @@ import statusBarImg from "../assets/status-bar-in-skill.png";
 import styles from "./status-bar.css";
 
 const StatusBar = () => {
+
+    // 日付を取得
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    // スキル値を計算
+    const skill = (year + month + day + hours + minutes + seconds);
+
     return (
         <>
             <img src={statusBarImg} className={styles.statusBar} alt="status-bar" draggable={false} />
             <div className={`${styles.grid} ${styles.statusText}`}>
-                <div className={styles.skill}>100</div>
+                <div className={styles.skill}>{skill}</div>
                 <div>Q23079</div>
             </div>
         </>
