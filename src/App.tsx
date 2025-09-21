63426aae19fccbe5b9e9f830a5fbfa88241eeacf
e1q23079 <e1q23079@oit.ac.jp>
2025-09-21 13:12:41 +0900

    ルーティング設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import { backScreen } from './style.css.ts'
-import FadeIn from './animation/back_screen/FadeIn.tsx'
-
-import StartBtn from './components/start-btn.tsx'
-
-import back_screen01 from './assets/back_screen01.png'
-import back_screen02 from './assets/back_screen02.png'
+import { Routes, Route, HashRouter } from "react-router-dom"
+import StartPage from "./page/start-page"
+import HomePage from "./page/home-page"
 
 const App = () => {
   return (
-    <>
-      {/* 昼 */}
-      <img src={back_screen01} alt="start screen back 01" className={backScreen} draggable={false} />
-      {/* 夜 */}
-      <FadeIn>
-        <img src={back_screen02} alt="start screen back 02" className={backScreen} draggable={false} />
-      </FadeIn >
-      <StartBtn></StartBtn>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </HashRouter>
   )
 }
 
