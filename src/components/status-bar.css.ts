8a966695f53f24528b4ddb0a43d7fd9aa44513e6
e1q23079 <e1q23079@oit.ac.jp>
2025-09-21 17:55:33 +0900

    経験値を表示

--- src/components/status-bar.css.ts
+++ src/components/status-bar.css.ts
@@ -1,8 +1,31 @@
 import { style } from "@vanilla-extract/css";
 
 const statusBar = style({
+    position: "relative",
     width: "45vw",
 });
-const styles = { statusBar };
+
+const statusText = style({
+    fontWeight: "bold",
+});
+
+const skill = style({
+    color: "white",
+});
+
+const grid = style({
+    position: "absolute",
+    fontSize: "2.5vw",
+    top: "0",
+    left: "0",
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr",
+    alignItems: "center",
+    justifyItems: "center",
+    width: "100%",
+    height: "100%",
+});
+
+const styles = { statusBar, grid, skill, statusText };
 
 export default styles;
--- src/components/status-bar.tsx
+++ src/components/status-bar.tsx
@@ -1,9 +1,15 @@
-import statusBarImg from "../assets/status-bar.png";
+import statusBarImg from "../assets/status-bar-in-skill.png";
 import styles from "./status-bar.css";
 
 const StatusBar = () => {
     return (
-        <img src={statusBarImg} className={styles.statusBar} alt="status-bar" draggable={false} />
+        <>
+            <img src={statusBarImg} className={styles.statusBar} alt="status-bar" draggable={false} />
+            <div className={`${styles.grid} ${styles.statusText}`}>
+                <div className={styles.skill}>100</div>
+                <div>Q23079</div>
+            </div>
+        </>
     )
 };
 
