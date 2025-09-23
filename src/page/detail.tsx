3957f3c1db10667b87e102c98be6f47926827bcd
e1q23079 <e1q23079@oit.ac.jp>
2025-09-23 11:45:59 +0900

    詳細ページ_基本基盤

--- src/page/detail.tsx
+++ src/page/detail.tsx
@@ -1,5 +1,35 @@
+import BackBtn from "../components/back-btn";
+import { backBtnStyle, statusBarStyle } from "./home.css";
+
+import back from "../assets/back.png";
+import { backScreen } from "./start.css";
+import StatusBar from "../components/status-bar";
+
+import FadeOutRedirect from "../animation/FadeOutRedirect";
+import { useState } from "react";
+import FadeInSc from "../animation/FadeInSc";
+
 const DetailPage = () => {
-    return <div>Detail Page</div>;
+
+    const [status, setStatus] = useState(false);
+
+    const backBtn = () => {
+        setStatus(true);
+    }
+
+    return (
+        <FadeInSc>
+            <FadeOutRedirect redirectUrl="#home" trigger={status}>
+                <img src={back} alt="start screen back 02" className={backScreen} draggable={false} />
+                <div className={backBtnStyle} onClick={backBtn}>
+                    <BackBtn></BackBtn>
+                </div>
+                <div className={statusBarStyle}>
+                    <StatusBar></StatusBar>
+                </div>
+            </FadeOutRedirect>
+        </FadeInSc >
+    )
 }
 
 export default DetailPage;
