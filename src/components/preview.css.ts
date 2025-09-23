fba0774ef201048991fb8483e10611f3ab9ff3ab
e1q23079 <e1q23079@oit.ac.jp>
2025-09-23 22:49:33 +0900

    プレビューページ作成

--- src/components/preview.css.ts
+++ src/components/preview.css.ts
@@ -1,9 +1,7 @@
 import { style } from "@vanilla-extract/css";
 
 const previewStyle = style({
-    height: "100vh",
     padding: '10px',
-    overflow: "scroll",
 });
 
 export default previewStyle;
